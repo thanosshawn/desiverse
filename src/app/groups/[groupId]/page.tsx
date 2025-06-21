7ba4c40cd4c2b4c69d8dabaa3722430881c0a1f6
thanos shawn <thanosshawn@gmail.com>
2025-06-21 13:43:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/groups/[groupId]/page.tsx
+++ src/app/groups/[groupId]/page.tsx
@@ -3,7 +3,7 @@
 
 import { Header } from "@/components/layout/header";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Loader2, Users } from "lucide-react";
 import Link from "next/link";
 import React from "react";
